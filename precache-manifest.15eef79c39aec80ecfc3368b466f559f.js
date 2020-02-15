62a00fad2d386814ee108565e85956b6eddc267e
ltdhien <ltdhien@hotmail.com>
2020-02-15 22:00:53 +0700

    Updates

---
+++ precache-manifest.15eef79c39aec80ecfc3368b466f559f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ee17bc752431eb37220dbbab2b479e",
+    "url": "/video-search/index.html"
+  },
+  {
+    "revision": "f8748380de06102f96f5",
+    "url": "/video-search/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b7f2055e4f2f966528ab",
+    "url": "/video-search/static/js/2.c48c3bd2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/video-search/static/js/2.c48c3bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8748380de06102f96f5",
+    "url": "/video-search/static/js/main.99e5d7e9.chunk.js"
+  },
+  {
+    "revision": "8471c0a760c99915f47d",
+    "url": "/video-search/static/js/runtime-main.666315d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/video-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
